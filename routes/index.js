5fb5530843b2cf5cd76eaaa0f2c0c79ecdb06c57
sslover <samjslover@gmail.com>
2015-11-15 00:50:51 -0500

    adding pinterest

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,9 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-
-
+// PINTEREST SETUP //
+var pinterestAPI = require('pinterest-api');
+ 
 // home route
 router.get('/', function(req, res) {
   
@@ -36,14 +37,74 @@ router.get('/api/get/twitter/:user', function(req,res){
   console.log(requestedScreeName);
 
   client.get('statuses/user_timeline', {screen_name: requestedScreeName}, function(error, tweets, response){
-    if (!error) {
-      console.log(tweets);
-    }
-
     console.log(tweets);
-
     res.json(tweets);
   });  
+
+})
+
+router.get('/api/get/pinterest/boards/:account', function(req,res){
+
+  var requestedAccount = req.params.account;
+
+  // Create a new object and set the accountname 
+  var pinterest = pinterestAPI(requestedAccount);
+
+
+  // Get all boards for the above account name 
+  pinterest.getBoards(true, function (boards) {
+    console.log(boards);
+    res.json(boards);      
+  }); 
+
+})
+
+router.get('/api/get/pinterest/pins/:account', function(req,res){
+
+  var requestedAccount = req.params.account;
+
+  // Create a new object and set the accountname 
+  var pinterest = pinterestAPI(requestedAccount);
+
+
+  // Get all boards for the above account name 
+  pinterest.getPins(function (pins) {
+    console.log(pins);
+    res.json(pins);      
+  }); 
+
+})
+
+router.get('/api/get/pinterest/account', function(req,res){
+
+  var requestedAccount = req.params.account;
+
+  // Create a new object and set the accountname 
+  var pinterest = pinterestAPI(requestedAccount);
+
+
+  // Get all boards for the above account name 
+  pinterest.getBoards(true, function (boards) {
+    console.log(boards);
+    res.json(boards);      
+  }); 
+
+})
+
+router.get('/api/get/pinterest/pins/:account/board/:board', function(req,res){
+
+  var requestedAccount = req.params.account;
+  var requestedBoard = req.params.board;
+
+  // Create a new object and set the accountname 
+  var pinterest = pinterestAPI(requestedAccount);
+
+
+  // Get pins from a board (second parameter determines whether you want the results paginated and to include some metadata) 
+  pinterest.getPinsFromBoard(requestedBoard, true, function (pins) {
+    console.log(pins);
+    res.json(pins);
+  });
 
 })
 
